25f1930bee5a77dc6455dea7b6a7d6ceb4479b7d
carlomz <carlomz@msn.com>
2018-02-20 00:04:52 +0000

    Update pstest.ps1

--- pstest.ps1
+++ pstest.ps1
@@ -1,3 +1,17 @@
-Write-Host "Hello"
-ping 8.8.8.8
-Write-Host "World!"
+$file = "C:\test.txt"
+
+Try {
+    If (!(Test-Path $file)) {
+        New-Item $file -ItemType file
+        Write-Host "Created new file and text content added"
+        Add-Content -Path $file -Value "$((Get-Date).ToString('MM/dd/yyyy HH:mm:ss')) $(Get-Random -Maximum 10)"
+    } Else {
+        Add-Content -Path $file -Value "$((Get-Date).ToString('MM/dd/yyyy HH:mm:ss')) $(Get-Random -Maximum 10)"
+        Write-Host "File already exists and new text content added"
+    }
+}
+Catch {
+    $ErrorMessage = $_.Exception.Message
+    Write-Host $ErrorMessage
+    Exit -1
+}
